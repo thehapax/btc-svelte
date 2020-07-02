1516625349d27be7f63d9c176f9d45dd9dad8955
thehapax <39811582+thehapax@users.noreply.github.com>
2020-07-02 14:23:54 -0700

    add rss ycomb feed for testing

--- src/App.svelte
+++ src/App.svelte
@@ -1,15 +1,19 @@
-<h2>Data Table</h2>
-
 <svelte:head>
 	<link rel="stylesheet" href="./dark-theme.css">
 	<title>Data Table - SMUI</title>
 </svelte:head>
 
+<h2>Data Table H2</h2>
+
 <h1>Hello world! Test here</h1>
 
 <script>
 	let name = '';
+	import RssFeed from './RssFeed.svelte';
 </script>
 
 <input bind:value={name} placeholder="enter your name">
 <p>Hello {name || 'stranger'}!</p>
+<hr>
+
+<RssFeed />
